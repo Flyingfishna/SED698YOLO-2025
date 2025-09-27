8bf89632a6218b80b6be51c622dbc234bb0a6c4a
Flyingfishna <47020083+Flyingfishna@users.noreply.github.com>
2025-09-27 15:05:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ st.title("YOLO Image Detection App :)")
 
 # Load YOLO model
 # model = YOLO("runs/detect/train73/weights/best.pt")
-model = YOLO("yolo11n.pt")
+# model = YOLO("yolo11n.pt")
+model = YOLO("best1.pt")
 # Upload image
 uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jpeg", "png"])
 
@@ -35,4 +36,4 @@ if uploaded_image is not None:
   
   # Count people
   person_count = class_names.count("person")
-  st.write(f"Number of people detected: **{person_count}**")
+  st.write(f"Number of plate detected: **{person_count}**")
