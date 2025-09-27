cbac98a4975486aba08d5ab8e1da8f71c4c290c4
Flyingfishna <47020083+Flyingfishna@users.noreply.github.com>
2025-09-27 15:07:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,6 @@ if uploaded_image is not None:
   class_names = [model.names[i] for i in class_ids]
   
   # Count people
-  person_count = class_names.count("person")
+  # person_count = class_names.count("person")
+  person_count = class_names.count("Plate")
   st.write(f"Number of plate detected: **{person_count}**")
